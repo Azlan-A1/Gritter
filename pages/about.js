76687a696ec95bfea5092d9bacdf0e2f4861e6bc
Azlan <azlanahmad029@gmail.com>
2023-09-23 15:53:03 -0400

    3:53pm

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AboutPage() {
+    return (
+        <div>
+            <h1>About Gritter</h1>
+            <p>
+                Gritter is an innovative platform designed to [Your Description Here].
+            </p>
+            <p>
+                Our mission is to [Your Mission Statement Here].
+            </p>
+        </div>
+    );
+}
+
+export default AboutPage;

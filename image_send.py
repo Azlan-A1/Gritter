061e36211bb71f0e780975b8e66a04ddd8f7f66c
Eric Augustinepillai <eriqaugu@gmail.com>
2023-09-23 16:13:09 -0400

    Update image_send.py
    Added showing cost of last 20 NFT transactions in ethereum and usdc.

--- image_send.py
+++ image_send.py
@@ -25,11 +25,17 @@ query MyQuery( $address: Address) {
           large
         }
       }
-      nftSaleTransactions {
-        nfts {
-          tokenAmount
-        }
-      }
+      
+    }
+  }
+  NFTSaleTransactions(
+    input: {filter: {nfts: {tokenAddress: {_eq: $address}}}, blockchain: ethereum, limit: 20, order: {blockTimestamp: DESC}}
+  ) {
+    NFTSaleTransaction {
+      formattedPaymentAmountInNativeToken
+      formattedPaymentAmountInUSDC
+      transactionHash
+      blockTimestamp
     }
   }
 }            
@@ -43,16 +49,56 @@ query MyQuery( $address: Address) {
     current_token_address = []
     tokenId = []
     image = []
+    transactionhash = []
+    blocktimestamp = []
+    payment_eth = []
+    payment_usdc = []
+    average_eth = 0
+    average_usdc = 0
     for search_result_index in range(0,len(query_response.data['TokenNfts']['TokenNft'])):
         current_token_address.append(query_response.data['TokenNfts']['TokenNft'][search_result_index]['address'])
         tokenId.append(query_response.data['TokenNfts']['TokenNft'][search_result_index]['tokenId'])
         image.append(query_response.data['TokenNfts']['TokenNft'][search_result_index]['contentValue']['image']['large'])
+    for search_result_index in range(0, len(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"])):
+        transactionhash.append(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"][search_result_index]["transactionHash"])
+        blocktimestamp.append(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"][search_result_index]["blockTimestamp"])
+        payment_eth.append(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"][search_result_index]["formattedPaymentAmountInNativeToken"])
+        payment_usdc.append(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"][search_result_index]["formattedPaymentAmountInUSDC"])
+        average_eth+=payment_eth[search_result_index]
+        average_usdc+=payment_usdc[search_result_index]
+
+    average_eth = average_eth/len(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"])
+    average_usdc = average_usdc/len(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"])
     print(current_token_address)
     print(tokenId)
     print(image)
+    print("\n\n\n")
+    print(payment_usdc)
+    print("\n\n\n")
+    print(average_eth)
+    print(average_usdc)
     print("\n\n\n")
     print(query_response.data)
     return image[0]
 
 asyncio.run(name_send("0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"))
 
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
+# @app.route('/search', methods=['POST'])
+# def search():
+    
+#     search_term = request.form.get('search_term')
+#     print("Search term entered:", search_term)
+#     image_url = asyncio.run(name_send(search_term))
+
+#     return "search term received: "+search_term+ " image url is: " + image_url + render_template('search_results.html', search_term=search_term, image_url=image_url)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)

6ca6082066d27c183c9f0217fd3cc2ea9e88c15c
Eric Augustinepillai <eriqaugu@gmail.com>
2023-09-24 08:02:06 -0400

    Short code behind a front end and backend
    Shows off the query ability of Airstack and returns specific values the user would want in different situations.

--- image_send.py
+++ image_send.py
@@ -1,19 +1,21 @@
 from airstack.execute_query import AirstackClient
 import asyncio
 
-#from flask import Flask, request, render_template
+from flask import Flask, request, render_template
 
 
 
 address = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
 
-async def name_send(address):
+async def name_send(address,name="a"):
     
     api_client = AirstackClient(api_key = "ef3d1cdeafb642d3a8d6a44664ce566c")
     #Uses demo key from python SDK files
     query="""
-query MyQuery( $address: Address) {
-  TokenNfts(input: {blockchain: ethereum, filter: {address: {_eq: $address}}}) {
+query MyQuery($address: Address, $name: String) {
+  TokenNfts(
+    input: {blockchain: ethereum, filter: {address: {_eq: $address}, _or: {metaData: {name: {_eq: $name}}}}}
+  ) {
     TokenNft {
       tokenId
       metaData {
@@ -26,7 +28,6 @@ query MyQuery( $address: Address) {
           large
         }
       }
-      
     }
   }
   NFTSaleTransactions(
@@ -39,16 +40,18 @@ query MyQuery( $address: Address) {
       blockTimestamp
     }
   }
-}            
+}      
 """
 #Query uses the AI integration on the airstack search model to collect information on the nft's image, token id, token address, blockchain status, how much was paid during transactions of the nfts including the block time stamp and transaction hash.
     variables =  {
-       "address":address
+       "address":address,
+       "name":name
 }
     
     execute_query_client  = api_client.create_execute_query_object(query = query, variables = variables) 
     
     query_response = await execute_query_client.execute_query()
+    #print(query_response.data)
     #execute_query turns the object into readable data
     if query_response is None:
         return "There are no nft's in this address"
@@ -118,18 +121,42 @@ query MyQuery( $address: Address) {
         average_eth = average_eth/len(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"])
         average_usdc = average_usdc/len(query_response.data["NFTSaleTransactions"]["NFTSaleTransaction"])
     
-    print(current_token_address)
-    print(tokenId)
-    print(image)
-    print("\n\n\n")
-    print(payment_usdc)
-    print("\n\n\n")
-    print(average_eth)
-    print(average_usdc)
-    print("\n\n\n")
-    print(transactionhash)
-    print("\n\n\n")
-    print(query_response.data)
-    return image[0]
+    # print(current_token_address)
+    # print(tokenId)
+    # print(image)
+    # print("\n\n\n")
+    # print(payment_usdc)
+    # print("\n\n\n")
+    # print(average_eth)
+    # print(average_usdc)
+    # print("\n\n\n")
+    # print(transactionhash)
+    # print("\n\n\n")
+    # print(query_response.data)
+    # print("\n\n\n")
+    # print(average_eth)
+    return image[0],tokenId[1],average_eth
 
-asyncio.run(name_send(address))
+print(asyncio.run(name_send("0x9340204616750cb61e56437bEfC95172C6Ff6606","FarCats #1214")))
+
+app = Flask(__name__,template_folder="templates")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    
+    search_term = request.form.get('search_term')
+    name_term = request.form.get('name_term')
+    print("Search term entered:", search_term+" "+name_term)
+    list = asyncio.run(name_send(search_term,name_term))
+    image_url = list[0]
+    tokenId = list[1]
+    average_eth = list[2]
+
+    return "search term received: "+search_term+ " Name term was: "+name_term+"  "+ "image url is: " + image_url +  "\ntoken id is: "+str(tokenId)+"\taverage ethereum transaction over 20 transactions was: "+str(average_eth) + "\n" +render_template('search_results.html', search_term=search_term, image_url=image_url) 
+if __name__ == '__main__':
+    app.run(debug=True)
